c08fc6f66ce288bfc6a8de3295cb6ac677d5f731
Arne Neumann <github@arne.cl>
2014-11-21 16:06:53 +0100

    added bash launch script for convenience (can be symlinked from /usr/bin)

---
+++ rsttool.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# reliably get the path to the directory this script is stored
+# (includes magic for symlink resolution)
+# taken from: http://stackoverflow.com/a/246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+
+wish $DIR/LoadRSTTool.tcl &
